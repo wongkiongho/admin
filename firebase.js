b11b6536f0ba5ba95478c8ee12f27b23bf9a9eae
Wong Kiong Ho <kiongho.wong@beans.com.my>
2024-05-03 11:38:20 +0800

    commit

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFAs5FAushtnOmttaYMmJ0Kk09wzbPRrI",
+  authDomain: "admin-6e2fb.firebaseapp.com",
+  projectId: "admin-6e2fb",
+  storageBucket: "admin-6e2fb.appspot.com",
+  messagingSenderId: "324915407706",
+  appId: "1:324915407706:web:3d0a2dc8510047ae622163",
+  measurementId: "G-TXW87WJZC7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export { auth, GoogleAuthProvider, signInWithPopup };
